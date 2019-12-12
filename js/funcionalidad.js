c16e76563944ce63c04db2d22fd7367fdf3a55c0
cbascordoba14 <cbascordoba14@gmail.com>
2019-12-11 21:37:09 -0500

    41. Iluminando la secuenta, se ilumina la secuencia comparando con un switch el valor del array y retornando el color, tambien se evidencia que con let, la variable conservará el valor dentro de cada iteración

--- js/funcionalidad.js
+++ js/funcionalidad.js
@@ -1,7 +1,7 @@
 
       const azul = document.getElementById('azul')
       const rojo = document.getElementById('rojo')
-      const amarilla = document.getElementById('amarilla')
+      const amarillo = document.getElementById('amarillo')
       const verde = document.getElementById('verde')
       const btnEmpezar = document.getElementById('btnEmpezar')
 
@@ -9,16 +9,17 @@
         constructor() {
           this.inicializar()
           this.generarSecuencia()
+          this.siguienteNivel()
         }
 
         inicializar() {
           btnEmpezar.classList.add('hide')
-          this.nivel = 1
+          this.nivel = 5
           this.colores = {
             //   azul:azul es lo mismo que azul solo
             azul,
             rojo,
-            amarilla,
+            amarillo,
             verde
 
           }
@@ -33,6 +34,43 @@
             //math.radom genera un valor entre 0-1 al multiplicarlo por 4 quedara entre 0 y 4
             this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
         }
+
+        //Funcion que va a llamar a iluminar frecuencia
+        siguienteNivel(){
+            this.iluminarSecuencia()
+        }
+
+        transformarNumeroAColor(numero){
+            switch (numero){
+                case 0:
+                    return 'azul'
+                case 1:
+                    return 'rojo'
+                case 2:
+                    return 'amarillo'
+                case 3:
+                    return 'verde'
+            }
+        }
+
+        //vamos a recorrer el array de la secuencia hasta el nivel que este el usuario
+        iluminarSecuencia(){
+            for(let i =0; i < this.nivel; i++){
+                //con let, conservará su valor dentro de cada iteración.
+                const color = this.transformarNumeroAColor(this.secuencia[i])
+                setTimeout(()=> this.iluminarColor(color),1000* i)
+            }
+        }
+
+        iluminarColor(color){
+            this.colores[color].classList.add ('light')
+            setTimeout(()=> this.apagarColor(color),350)
+        }
+
+        apagarColor(color){
+            this.colores[color].classList.remove('light')
+        }
+
       }
 
       function empezarJuego() {
